2bfeebdbd8bea056ed4976434336ff82bccfff29
dparret <damienparret@hotmail.com>
2025-01-11 17:17:31 +0100

    Update repo link on footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => 
   <footer className="text-neutral-500 row-start-3 flex gap-1 flex-wrap items-center justify-center">
-    <a className="hover:underline" href='https://github.com/dparret/simple-race-timer'>Source code available on GitHub</a>
+    <a className="hover:underline" href='https://github.com/dparret/racing-weight-points-tracker'>Source code available on GitHub</a>
   </footer>
 
 export default Footer;
